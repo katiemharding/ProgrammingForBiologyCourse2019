9e721eab5a6f457a1ed9191f6d5e579414442763
info <info@info14.cshl.edu>
2019-10-16 15:58:27 -0400

    fix numeric problem with sys.argv

--- problemsets/PythonProblemSets/True_ifelse.py
+++ problemsets/PythonProblemSets/True_ifelse.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
-unknown_number = sys.argv[1]
-if unknown_number >50:
+unknown_number = int(sys.argv[1])
+if bool( unknown_number >50):
 	print(unknown_number, "is greater than 50")
 else:
 	print(unknown_number, "is less than 50")
