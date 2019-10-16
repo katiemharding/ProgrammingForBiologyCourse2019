3f6822ace5424ed762fff180b96b9984bb9554a0
info <info@info14.cshl.edu>
2019-10-16 16:08:46 -0400

    adding equals

--- problemsets/PythonProblemSets/positiveOrNegative.py
+++ problemsets/PythonProblemSets/positiveOrNegative.py
@@ -3,6 +3,8 @@ import sys
 unknown_number = int(sys.argv[1])
 if unknown_number>0:
 	print(unknown_number, "is positive")
+elif unknown_number == 0:
+	print("equals 0")
 else:
 	print(unknown_number, "is negative")
 
