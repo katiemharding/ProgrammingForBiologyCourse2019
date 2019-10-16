de8b817df04d53b251c0f8e316d002e1ca6f5bd2
info <info@info14.cshl.edu>
2019-10-16 15:53:12 -0400

    first version

---
+++ problemsets/PythonProblemSets/True_ifelse.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+unknown_number = sys.argv[1]
+if unknown_number >50:
+	print(unknown_number, "is greater than 50")
+else:
+	print(unknown_number, "is less than 50")
+
