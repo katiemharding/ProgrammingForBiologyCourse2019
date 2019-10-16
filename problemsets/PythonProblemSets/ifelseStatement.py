406ac69f6dfd74c84fc426941b760d200b869120
info <info@info14.cshl.edu>
2019-10-16 15:36:38 -0400

    first version of ifelse

---
+++ problemsets/PythonProblemSets/ifelseStatement.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+
+dna = 'GTACCTTGATTTCGTATTCTGAGAGGCTGCTGCTTAGCGGTAGCCCCTTGGTTTCCGTGGCAACGGAAAA'
+if 'ATG' in dna:
+	print("ATG is present")
+else:
+	print("ATG is not found")
+
