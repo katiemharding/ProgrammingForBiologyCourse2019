b7e11b2b9d3a683916276f632000ad83e97f40fb
info <info@info14.cshl.edu>
2019-10-16 16:34:05 -0400

    adding divisible by 3

--- problemsets/PythonProblemSets/positiveOrNegative.py
+++ problemsets/PythonProblemSets/positiveOrNegative.py
@@ -6,9 +6,16 @@ unknown_number = int(sys.argv[1])
 if unknown_number>0:
 	print(unknown_number, "is positive")
 	if unknown_number > 50:
-		print(unknown_number, "is bigger than 50")
+# modulo (%) returns the remainder.  allows testing if divisible by
+		if unknown_number%3 == 0:
+			print(unknown_number, "is larger than 50 and divisible by 3")
+		else:
+			print(unknown_number, "is bigger than 50")
 	else:
 		print(unknown_number, "is less than 50")
+# modulo tests if a number is even.
+# it returns the remainder (the numbers remaining after division)
+# if it is zero then there is no remainder
 		if unknown_number%2 == 0:
 			print(unknown_number, "is even")
 		else:
