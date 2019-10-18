7c8c62ebbc14c47933cf9bde54f86354cdbab5a3
info <info@info14.cshl.edu>
2019-10-18 16:42:38 -0400

    works to create a dictionary.  need to clean up the gene id

--- problemsets/PythonProblemSets/problemset6/prob6testindividual.py
+++ problemsets/PythonProblemSets/problemset6/prob6testindividual.py
@@ -1,8 +1,20 @@
 #! /usr/bin/env python3
 
-with open('Python_06.seq.txt', 'r') as unk_seq:
-	for rawline in unk_seq:
+genes = {}
+gene_ID = "" 
+seq = ""
+with open('testhead.fasta', 'r') as unk_fasta:
+	for rawline in unk_fasta:
 		line = rawline.rstrip() # remove white space at end
-		gene_id, seq = line.split() # split on white space in the middle
-		print(">", gene_id, "\treverse seq\n",seq, sep ="")
+		# line_list  = line.split(">") # split on white space in the middle
+		if line[0] == '>':
+			gene_ID =  line
+
+		else:
+			seq = line
+			genes[gene_ID] = seq # because the gene and seq are called outside of the for loop
+			# they are availabe to fill the genes dict here
+print(genes)
+# this is good because it loads in each line one at a time, keeping memory use low
+	
 
