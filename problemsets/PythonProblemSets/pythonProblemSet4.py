bdad028751c5438dd77b2c714d044d6762295adf
info <info@info14.cshl.edu>
2019-10-17 21:29:55 -0400

    second questions

--- problemsets/PythonProblemSets/pythonProblemSet4.py
+++ problemsets/PythonProblemSets/pythonProblemSet4.py
@@ -23,4 +23,24 @@ print("My Faves after pop: ", myfav)
 joined = ",".join(myfav)
 print(joined,"this is really odd", sep= ", comment:")
 
+# 2) strings to list
+print("problem two, strings to list")
+taxa = 'sapiens, erectus, neanderthalensis'
+print(taxa)
+print(taxa[0], "the first letter")
+
+type(taxa)
+print(type(taxa), "its a string, I did it right")
+species = taxa.split(', ')
+print(species, "species list")
+print(species[1], "the first species")
+print(type(species), "I did it right again")
+species_sort = sorted(species)
+print(species_sort, "Sorted species")
+species_bylen = sorted(species, key = len)
+print(species_bylen, "sorted by number of characters")
+
+
+
+
 
