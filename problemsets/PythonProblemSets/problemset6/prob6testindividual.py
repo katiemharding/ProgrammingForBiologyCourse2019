0f398f1a15815671bfd3a5cd25fb5342f089f72b
info <info@info14.cshl.edu>
2019-10-18 12:06:02 -0400

    finished fastq problem

--- problemsets/PythonProblemSets/problemset6/prob6testindividual.py
+++ problemsets/PythonProblemSets/problemset6/prob6testindividual.py
@@ -1,5 +1,22 @@
 #! /usr/bin/env python3
 
-# number 3
+# number 4 for this they just want total lines, average line, total characters
+total_nt = 0
+line_count = 0
 
-with 
+with open('Python_06.fastq', 'r') as unk_file:
+	for rawline in unk_file:
+		line = rawline.rstrip()
+		# each gene has 4 things
+		# @ indicates ID
+		# no start indicates seq
+		# + indicates sequence ID (gene_id) from before
+		# quality
+		line_count += 1
+		nt_count = len(line)
+		total_nt +=nt_count
+	print("line count is:", line_count)
+	print("total count is:", total_nt)
+	ave_nt_line = total_nt/line_count
+	print("average nt per line:", ave_nt_line)
+
--- problemsets/PythonProblemSets/problemset6/pythonProb6.py
+++ problemsets/PythonProblemSets/problemset6/pythonProb6.py
@@ -32,4 +32,23 @@ with open('Python_06.seq.txt', 'r') as unk_seq:
 # on the command line type
 # python3 prob6testindividual.py > Python06_seq_dict.txt
 
+# number 4 for this they just want total lines, average line, total characters
+total_nt = 0
+line_count = 0
+
+with open('Python_06.fastq', 'r') as unk_file:
+	for rawline in unk_file:
+		line = rawline.rstrip()
+		# each gene has 4 things
+	    # @ indicates ID
+		# no start indicates seq
+        # + indicates sequence ID (gene_id) from before
+        # quality
+		line_count += 1 # add one per line
+		nt_count = len(line) # find length of line
+		total_nt +=nt_count # add lengths to get each line
+	print("line count is:", line_count)
+	print("total count is:", total_nt)
+ 	ave_nt_line = total_nt/line_count # average is totoal nt/line
+	print("average nt per line:", ave_nt_line)
 
