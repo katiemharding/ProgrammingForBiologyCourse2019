fb6827c9381425ffac587d6384f669e196f67739
info <info@info14.cshl.edu>
2019-10-17 22:37:30 -0400

    count with feeds

--- problemsets/PythonProblemSets/set4count.py
+++ problemsets/PythonProblemSets/set4count.py
@@ -4,7 +4,7 @@ import sys
 min_argv = int( min(sys.argv[1:]))
 max_argv = int( max(sys.argv[1:])) # this will prevent user errors
 
-myrange = [print(ran) for ran in range(max_argv)]
+myrange = [print(ran) for ran in range(min_argv, max_argv)]
 myrange
 
 
