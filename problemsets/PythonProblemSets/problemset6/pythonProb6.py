90620110a3e61d44e6da28402b6adddbd3378902
info <info@info14.cshl.edu>
2019-10-18 11:09:59 -0400

    finished prob 1 and 2

--- problemsets/PythonProblemSets/problemset6/pythonProb6.py
+++ problemsets/PythonProblemSets/problemset6/pythonProb6.py
@@ -6,9 +6,18 @@
 # this downloads and saves the file to your workspace
 # first open the file to python
 
-with open('Python_06.txt', 'r') as file_object:
+# problem 2) write the file to another file
+with open('Python_06.txt', 'r') as file_object, open('Python_06_uc.txt', 'w') as song_write:
 	for rawline in file_object:
 		line = rawline.rstrip()
 		Upper_line = line.upper()
 		print(Upper_line)
+	#if you indent to here is only prints the last line to file	
+		# song_write.write(Upper_line) this code prints as one long string with white space
+		#you have to indent twice to get the whole file printed
+		song_write.write(Upper_line +'\n')
+print("wrote Python_06_uc.txt")
+
+
+
 
