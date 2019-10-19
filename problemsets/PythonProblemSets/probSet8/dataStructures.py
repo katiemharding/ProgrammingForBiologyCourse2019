eba92d2464f221c4d486a2d91cdb421c7395ced7
info <info@info14.cshl.edu>
2019-10-19 12:00:46 -0400

    this makes a fasta file into a dictionary

--- problemsets/PythonProblemSets/probSet8/dataStructures.py
+++ problemsets/PythonProblemSets/probSet8/dataStructures.py
@@ -9,19 +9,19 @@ except:
 	print("Please provide Name")
 
 genes = {}
-gene_ID = ""
-seq = ""
+gene_ID = None
 with open(File_Name, 'r') as unk_fasta:
 	for rawline in unk_fasta:
 		line = rawline.rstrip() # remove white space at end
 	# line_list  = line.split(">") # split on white space in the middle
-		if line[0] == '>':
+		if line.startswith( '>'):
 			gene_info  =  line # now it needs cleanded up
 			gene_long  = gene_info.replace('>','') # remove the identifer
-			gene_ID = gene_long.split( ' ')[0] # remove everything after the tab
-        # note: the above returns a two item list, one before \t one after \t
+			gene_ID = gene_long.split( ' ')[0] # remove everything after the space
+			genes[gene_ID] = ''
+		# note: the above returns a two item list, one before \t one after \t
 		else:
-			seq = line
-			genes[gene_ID] = seq # because the gene and seq are called outside of the for loop
+			
+			genes[gene_ID] +=line # because the gene and seq are called outside of the for loop
         # they are availabe to fill the genes dict here
 print(genes)
