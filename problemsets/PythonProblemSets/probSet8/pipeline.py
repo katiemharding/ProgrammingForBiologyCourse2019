18e821f9a556f6b6706c215daa721250bbb674d0
info <info@info14.cshl.edu>
2019-10-19 21:12:20 -0400

    trying to make pipeline work

--- problemsets/PythonProblemSets/probSet8/pipeline.py
+++ problemsets/PythonProblemSets/probSet8/pipeline.py
@@ -2,4 +2,12 @@
 
 import subprocess
 
-subprocess.run('python3 calculate_GC_content.py dna1.txt', shell = True)
+output_file = "GCoutputfile.txt"
+with open(output_file, "w") as ofh:
+	oops = subprocess.check_call('python3' 'calculate_GC_content.py' 'dna1.txt', stdout = ofh)
+print(oops)
+#outputfile2 = "dnacut.txt"
+#with open(outputfile2, "w") as ofh:
+#	if oops == 0:
+#		oops2 = subprocess.check_call('python3' 'dna_cutting.py' 'dna1.txt', stdout = ofh)
+
