47536f7688947bf2c32ee583928a9b75521cfa0c
info <info@info14.cshl.edu>
2019-10-22 17:18:26 -0400

    cleaned things up

--- problemsets/workshops/specieshomology/makeHomologyTable.py
+++ problemsets/workshops/specieshomology/makeHomologyTable.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
+import sys
 
-base_file = 'ss_rand5-800_v_qfo'
+base_file = sys.argv[1]
+# these files have two headers.  I will run the file twice, once with one header, once with the other
 apend_list = ['_BL50.txt', '_BP62.txt', '_VT10.txt', '_VT160.txt',
 '_VT20.txt', '_VT40.txt', '_VT80.txt']
 
@@ -13,19 +15,17 @@ field_string = "queryid subjectid identity alignmentlength mismatches gapopens q
 # the field string (above) was copied from one output.
 fields = field_string.split(' ')
 # the above code is now seperted into individual column names
-print(fields)
 first_row = []
 
 for new in new_list:
 	with open(new, 'r') as file1:
 		for line in file1:
-			if line[0] =='#':
-				continue # if the line starts with (first character = 0) skip, continue to next line
+			if line[0] =='#': 
+				continue # if the line starts with '#' (first character = 0) skip, continue to next line
 			hit_data = dict(zip(fields, line.strip('\n').split('\t')))
-			print(hit_data)
-	# by striping on \n you remove the language after the \n.  by splitting on '\t' you get each 'column' 
+	# by striping on \n you remove the white space at the end.   by splitting on '\t' you get each 'column' 
 	# the inner dict will be each 'column name'as key with the data as value
-			hit_data['file']= new 
+			hit_data['file']= new # name the file and declare it to be "new" from line 20
 			first_row.append(hit_data)
 			break # this exits the if loop and leaves only the first row "selected"
 
