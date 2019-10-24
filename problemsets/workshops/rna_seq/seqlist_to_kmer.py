1132c1bb346fa9fd0a79e32e29e5215654430941
info <info@info14.cshl.edu>
2019-10-24 12:32:30 -0400

    now it's a function

--- problemsets/workshops/rna_seq/seqlist_to_kmer.py
+++ problemsets/workshops/rna_seq/seqlist_to_kmer.py
@@ -12,8 +12,20 @@ def cut_kmer(seq, num):
 		kmer = seq[count:count+num]
 		kmer_list.append(kmer)
 		count +=1
-	return(kmer_list)
+
+	kmer_dict = {} #empty dict
+	for kmer in kmer_list: 
+	# is this kmer in our dictionary?
+		if kmer in kmer_dict:
+    # if it is, lets increment our count
+			previous_count = kmer_dict[kmer]
+			new_count = previous_count + 1
+			kmer_dict[kmer] = new_count
+		else:
+    # if not, lets add this nt to our dictionary and make count = 1
+			kmer_dict[kmer] = 1;
+
+	return(kmer_dict)
 
 print(cut_kmer(sequence, number))
-
 
