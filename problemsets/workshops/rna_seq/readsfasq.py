099c0fe4d136858d9475686645a05760ef46a848
info <info@info14.cshl.edu>
2019-10-24 11:22:07 -0400

    my trial and the class solution

--- problemsets/workshops/rna_seq/readsfasq.py
+++ problemsets/workshops/rna_seq/readsfasq.py
@@ -5,10 +5,24 @@ import sys
 file_name = sys.argv[1]
 from Bio import SeqIO
 
-sequences = []
+sequences  = []
 
 for seq_record in SeqIO.parse(file_name, "fastq"):
-	sequences.append(seq_record.seq)
+	sequences.append((seq_record.seq))
+print(sequences) # this returns the sequence, but with other garbage around it
 
-print(sequences)
+
+# this is the solution presented in class:
+def seq_from_fastq:
+
+	sequences  = [] #create empty list
+	fh = open(file_name) # open file
+	counter = 0 # start counter
+	for line in fh: # for line in file
+		line = line.rstrip() # remove white space
+		counter += 1 # add one to counter to get running tally of the number of lines
+		if (counter %4 == 2) # the sequence is always line 2.  
+		# %4 means return the remainder of count/4
+		# this means it will return the second of each record
+			seqqences.append(line)
 
