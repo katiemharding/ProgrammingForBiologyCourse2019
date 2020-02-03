859a05d37dcb37a8f091d6082976e7930155ac83
Katie Harding <katiemharding@yahoo.com>
2020-02-03 13:39:10 -0800

    line counter works

--- problemsets/PythonProblemSets/regularexpressions_P07.py
+++ problemsets/PythonProblemSets/regularexpressions_P07.py
@@ -10,9 +10,11 @@ def find_line(expression, text):
 		for rawline in new_text:
 			line = rawline.rstrip()
 			found = re.findall(expression, line)
-			line_count =+1 # each line gets a count
 			if len(found) >0:
 				line_list.append(line_count)
+				line_count += 1 # each line gets a count
+			else:
+				line_count += 1 # each line gets a count
 	return(line_list)
 
 if __name__ == '__main__':
