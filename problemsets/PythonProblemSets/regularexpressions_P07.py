fd9ea0284ef0ce4e2efa92ee57e203a2ce8f21df
Katie Harding <katiemharding@yahoo.com>
2020-02-03 13:51:20 -0800

    prints line, doesn not replace

--- problemsets/PythonProblemSets/regularexpressions_P07.py
+++ problemsets/PythonProblemSets/regularexpressions_P07.py
@@ -3,7 +3,7 @@
 import re
 import sys
 
-def find_line(expression, old_text, new_text):
+def find_line(expression, replacement, text):
 	line_list = [] # create empty list to store line marker
 	line_count = 0
 	with open(text, "r") as new_text:
@@ -12,7 +12,7 @@ def find_line(expression, old_text, new_text):
 			found = re.findall(expression, line)
 			if len(found) >0:
 				line_list.append(line_count)
-				line.replace(old_text, new_text)
+				line.replace(expression, replacement)
 				print(line)
 				line_count += 1 # each line gets a count
 			else:
@@ -24,8 +24,8 @@ if __name__ == '__main__':
 		print("please add expression and text to search")
 		exit(1)
 	expression = sys.argv[1]
-	text1 = sys.argv[2]
-	text2 = sys.argv[3]
-	output = find_line(expression, text)
+	replacement = sys.argv[2]
+	text = sys.argv[3]
+	output = find_line(expression, replacement, text)
 	print("matches found: ", output)
 	print("how many found: ", len(output))
