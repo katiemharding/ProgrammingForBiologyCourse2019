90f84ff238a846eb7378249d577a3c04f4625666
Katie Harding <katiemharding@yahoo.com>
2020-02-10 11:13:25 -0800

    now it writes to a file

--- problemsets/PythonProblemSets/regularexpressions_P07.py
+++ problemsets/PythonProblemSets/regularexpressions_P07.py
@@ -6,17 +6,21 @@ import sys
 def find_line(expression, replacement, text):
 	line_list = [] # create empty list to store line marker
 	line_count = 0
+	new_text = replacement + text # give the new file a name
+	new_file = open(new_text , "w")
 	with open(text, "r") as new_text:
 		for rawline in new_text:
 			line = rawline.rstrip()
 			found = re.findall(expression, line)
 			if len(found) >0:
 				line_list.append(line_count)
-				line.replace(expression, replacement) # this doesn't work.  
-				print(line)
+				new_file.write(line.replace(expression, replacement)) # add the line.  
 				line_count += 1 # each line gets a count
 			else:
 				line_count += 1 # each line gets a count
+				new_file.write(line)
+	new_file.close()
+	print("Wrote to file:", new_file) # it's nice to tell the user you wrote a file
 	return(line_list) # Line list is the location of each text.  
 
 if __name__ == '__main__':
